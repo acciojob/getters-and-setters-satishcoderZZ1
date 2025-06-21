097fb4cc7a0bbf8975c13ffa02a7a93fca5d353e
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-06-21 11:33:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Person {}
+class Person {
+	private name: string;
+	private age: number;
+	constructor(name: string, age: number){
+		this.name=name;
+		this.age=age;
+	}
 
-class Student extends Person {}
+	get name(): string{
+		return this.name;
+	}
 
-class Teacher extends Person {}
+	get age(): number{
+		return this.age;
+	}
+
+	set age(age: number){
+		this.age=age;
+	}
+	
+}
+
+class Student extends Person {
+	study(): void{
+		console.log(`${this.name} is studing`);
+	}
+}
+
+class Teacher extends Person {
+	teach(): void{
+		console.log(`${this.name} is teaching`);
+	}
+}
+
+// Creating a Person instance
 
 // Do not change the code below this line
 window.Person = Person;
